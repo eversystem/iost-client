8d7e8d65139e1c54012056e6ac24bac5bb7dc54e
takaowada <45811854+takaowada@users.noreply.github.com>
2025-09-09 18:25:58 +0900

    [Fix] eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,14 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [
+    '.eslintrc.js',
+    'node_modules/**',
+    'build/**',
+    '.yarn/**',
+    '**/build/**',
+    '**/.yarn/**'
+  ],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
